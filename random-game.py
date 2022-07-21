5e1e29b636c3d8780222e22f7118cc3b62e8bb72
christina <houxiaolin345@163.com>
2022-07-21 14:52:42 +0800

    add guess time

--- random-game.py
+++ random-game.py
@@ -1,7 +1,10 @@
 import random
 r = random.randint(1,100)
+i = 0 # count of guess
 while True:
+	print("This is", i , "times to guess")
 	input_r = input("Please enter a number \n")
+	i = i + 1
 	input_r = int(input_r)
 	if r == input_r:
 		print("You guess right!")
